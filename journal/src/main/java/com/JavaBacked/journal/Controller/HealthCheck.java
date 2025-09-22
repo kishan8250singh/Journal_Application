13fa845e431f2d2582298f266339b0fa722f858a
Kishan Singh <kishansingh121128@gmail.com>
2025-09-22 18:29:32 +0530

    rename method

--- journal/src/main/java/com/JavaBacked/journal/Controller/HealthCheck.java
+++ journal/src/main/java/com/JavaBacked/journal/Controller/HealthCheck.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheck {
     @GetMapping("/health-check")
-    public  String healthcheck(){
+    public  String Healthcheck(){
         return "ok";
     }
 }
